3d0a385300a9d91617f7aa9c84deb9bb88b7c977
BigDeadNet <dark.xf12@gmail.com>
2025-10-27 01:52:17 +0300

    init: add tictactoe.cpp

---
+++ tictactoe.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void drawBoard(char board[3][3]) {
+    cout << "-------------" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "| ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl << "-------------" << endl;
+    }
+}
+
+int main() {
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    drawBoard(board);
+    return 0;
+}
